ccebaa07d0831550bda5df9f2b498bc3f81c730f
takuminaoya <okacool32@gmail.com>
2025-04-25 18:30:23 +0800

    v1.0.1-dev

---
+++ configs/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+
+    // jika lokal ini posisi atau disk dimana whatsapp.json tersimpan
+    "disk" => "local",
+    "path" => "modules",
+    "filename" => "whatsapp.json",
+
+    // jika produksi ini posisi atau disk dimana whatsapp.ky tersimpan
+    "public_disk" => "assets",
+    "public_path" => "configuration",
+    "public_name" => "whatsapp.key",
+
+    // Base url whapify
+    "base_url" => "https://whapify.id/api",
+];
